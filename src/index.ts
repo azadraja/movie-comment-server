c8278742ea49772a03d6b78c658c6fe31efad2fa
Raja Avinash <rajaavinash.a@gmail.com>
2024-03-22 16:27:35 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import GraphQLJSON from "graphql-type-json";
+import { resolvers as commentResolvers } from "./resolvers/resolvers";
+import { ApolloServer } from "@apollo/server";
+import { typeDefs } from "./schema/schema";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+
+dotenv.config();
+
+const resolvers = {
+  JSON: GraphQLJSON,
+  ...commentResolvers,
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+(async () => {
+  await server.start();
+  app.use("/api/graphql", cors(), express.json(), expressMiddleware(server));
+})();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
